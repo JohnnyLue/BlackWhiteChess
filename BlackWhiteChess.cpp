5ad55cf5ea0f229c5dcaaa45a40823779a88cc90
JohnnyLue <90609939+JohnnyLue@users.noreply.github.com>
2022-12-18 15:34:44 +0800

    new version, add gui

--- BlackWhiteChess.cpp
+++ BlackWhiteChess.cpp
@@ -1,16 +1,231 @@
-﻿// BlackWhiteChess.cpp: 定義應用程式的進入點。
-//
+﻿
+#include <SFML/Graphics.hpp>
 
-#include "BlackWhiteChess.h"
+#include "GameEngine.h"
 
-using namespace std;
+using namespace sf;
 
 int main()
 {
-	gameEngine* game = new gameEngine();
-	
-	while (!game->gameEnd())
-		game->runGame();
+    do {
+        gameEngine* game = new gameEngine();
+        AI* ai = nullptr;
+        RenderWindow window(VideoMode(297 * 2, 297 * 2), "The Game!", sf::Style::Titlebar | sf::Style::Close);
 
-	return 0;
+        
+        Texture t1, t2, t3, t4;
+        t1.loadFromFile("images/chessBoard.jpg");
+        t2.loadFromFile("images/chess2.jpg");
+        t3.loadFromFile("images/flip2.jpg");
+        t4.loadFromFile("images/winner2.jpg");
+        Sprite backGround(t1);
+        backGround.setScale(Vector2f(2.0, 2.0));
+
+        Sprite c(t2);
+        c.setScale(Vector2f(2.0, 2.0));
+        Sprite ani(t3);
+        ani.setScale(Vector2f(2.0, 2.0));
+
+        int choose = -1;
+
+        window.setVisible(false);
+
+        while (choose != 1 && choose != 2)
+        {
+            std::string t;
+            cout << "Choose mode (1)AI or (2)two players:";
+            cin >> t;
+            choose = atoi(t.c_str());
+            system("CLS");
+        }
+
+        if (choose == 1)
+        {
+            choose = -1;
+            while (choose != 1 && choose != 2)
+            {
+                std::string t;
+                cout << "you want to be 1(black) or 2(white):";
+                cin >> t;
+                choose = atoi(t.c_str());
+                system("CLS");
+            }
+            int type;
+            if (choose == 1)
+                type = game->WHITE;
+            else
+                type = game->BLACK;
+
+            ai = new AI(game, type);
+        }
+
+        int oldBoard[8 * 8];
+        int firstIn = true;
+
+        window.setVisible(true);
+
+        while (!game->gameEnd())
+        {
+            if (ai)
+            {
+                if (game->state == game->BLACK_GO && ai->type == game->BLACK)
+                {
+                    ai->go();
+                }
+
+                if (game->state == game->WHITE_GO && ai->type == game->WHITE)
+                {
+                    ai->go();
+                }
+            }
+
+            Event e;
+            while (window.pollEvent(e))
+            {
+
+                if (e.type == Event::Closed)
+                    window.close();
+
+                if (e.type == Event::KeyPressed)
+                    if (e.key.code == Keyboard::Q)
+                    {
+                        game->state = game->state::GAME_END;
+                    }
+
+                if (e.type == Event::MouseButtonPressed)
+                {
+                    int x = e.touch.x, y = e.touch.y;
+
+                    c.setTextureRect(IntRect(33, 0, 33, 33));
+
+                    x -= 40, y -= 40;
+                    x = floor(x / 64);
+                    y = floor(y / 64);
+
+                    //c.setPosition(x, y);
+                    game->runGame(y, x);
+
+                }
+
+            }
+
+            if (firstIn)
+            {
+                memcpy(oldBoard, game->board, 64 * sizeof(int));
+                firstIn = false;
+            }
+
+            Time t;
+            Clock clock;
+
+            for (int f = 0; f < 7; f++)
+            {
+                while (t.asSeconds() < 0.05)
+                {
+                    t += clock.restart();
+                }
+                t = t.Zero;
+                window.draw(backGround);
+
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        ani.setPosition(40 + j * 64, 40 + i * 64);
+                        c.setPosition(40 + j * 64, 40 + i * 64);
+                        if (game->board[i * 8 + j] == game->WHITE)
+                        {
+                            if (oldBoard[i * 8 + j] == game->BLACK)//ate anime
+                            {
+                                ani.setTextureRect(IntRect(33 * f, 33, 33, 33));
+                                window.draw(ani);
+                            }
+                            else
+                            {
+                                c.setTextureRect(IntRect(0, 0, 33, 33));
+                                window.draw(c);
+                            }
+
+                        }
+                        else if (game->board[i * 8 + j] == game->BLACK)
+                        {
+                            if (oldBoard[i * 8 + j] == game->WHITE)//ate anime
+                            {
+                                ani.setTextureRect(IntRect(33 * f, 0, 33, 33));
+                                window.draw(ani);
+                            }
+                            else
+                            {
+                                c.setTextureRect(IntRect(33, 0, 33, 33));
+                                window.draw(c);
+                            }
+                        }
+                        else if (game->board[i * 8 + j] == game->CAN_GO)
+                        {
+                            if (game->state == game->WHITE_GO)
+                                c.setTextureRect(IntRect(66, 0, 33, 33));
+
+                            if (game->state == game->BLACK_GO)
+                                c.setTextureRect(IntRect(99, 0, 33, 33));
+
+                            window.draw(c);
+                        }
+                    }
+                }
+                window.display();
+            }
+
+            memcpy(oldBoard, game->board, 64 * sizeof(int));
+        }
+
+        Clock clock0;
+        while (clock0.getElapsedTime() < sf::seconds(1));
+
+        Sprite win(t4);
+        win.setOrigin(16, 16);
+        win.setPosition(40 + 64 * 4, 40 + 64 * 4);
+        win.scale(10, 10);
+        int winner = game->getWinner();
+        if (winner == game->WHITE)
+        {
+            win.setTextureRect(IntRect(0, 0, 33, 33));
+            window.draw(win);
+        }
+        if (winner == game->BLACK)
+        {
+            win.setTextureRect(IntRect(33, 0, 33, 33));
+
+            window.draw(win);
+        }
+        if (winner == game->NO_CHESS)
+        {
+            win.setTextureRect(IntRect(66, 0, 33, 33));
+
+            window.draw(win);
+        }
+        window.display();
+
+        Clock clock;
+        while (clock.getElapsedTime() < sf::seconds(2.5));
+
+        window.setVisible(false);
+
+        std::string a;
+        char firstC='0';
+        while (firstC != 'Q' && firstC != 'q' && firstC != 'Y' && firstC != 'y')
+        {
+            cout << "replay?(y or q):";
+            cin >> a;
+            if(!a.empty())
+                firstC = a.at(0);
+            system("CLS");
+        }
+        if (firstC == 'q' || firstC == 'Q')
+            break;
+
+    } while (1);
+
+    cout << "Thanks for playing.\n";
+
+    return 0;
 }
---
+++ GameEngine.h
@@ -0,0 +1,445 @@
+// BlackWhiteChess.h: 標準系統 include 檔案的 include 檔案，
+// 或專案專屬的 Include 檔案。
+
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <SFML/Audio.hpp>
+using std::cout;
+using std::cin;
+#include <iomanip>
+#include <vector>
+using std::vector;
+
+using std::min;
+using std::max;
+// TODO: 在此參考您的程式所需的其他標頭。
+class gameEngine {
+
+	sf::SoundBuffer buffer;
+	
+	sf::Sound sound;
+
+	enum {
+		HAVE_CHESS,
+		OUT_OF_BOUND,
+		CANT_PLACE,
+		VALID
+	};
+
+	enum {
+		BLACK_NO_PLACE,
+		WHITE_NO_PLACE,
+		NO_PLACE,
+		FINE
+	};
+
+public:
+	
+	enum state {
+		GAME_SETTING,
+		WHITE_GO,
+		BLACK_GO,
+		GAME_END
+	};
+
+	int state;
+
+
+	enum {
+		BLACK = -1,
+		NO_CHESS = 0,
+		WHITE = 1,
+		CAN_GO//not differ from black or white
+	};
+
+
+	int board[8 * 8];
+
+	gameEngine() {
+		memset(board, 0, 8 * 8 * sizeof(int));
+
+		board[3 * 8 + 3] = WHITE;
+		board[3 * 8 + 4] = BLACK;
+		board[4 * 8 + 3] = BLACK;
+		board[4 * 8 + 4] = WHITE;
+
+		state = GAME_SETTING;
+		runGame(-1, -1);
+
+		sf::RenderWindow window(sf::VideoMode(320, 480), "The Game!");
+
+		buffer.loadFromFile("audio/sound.wav");
+		sound.setBuffer(buffer);
+	}
+
+	void runGame(int x,int y)//col,row
+	{
+		if (state == GAME_SETTING)
+			state = BLACK_GO;
+
+		vector<int> ate;
+		int check;
+
+		switch (state) {
+		case BLACK_GO:
+			//printBoard();
+			check = checkBoard(BLACK);
+
+			if (checkValid(x * 8 + y) != VALID)
+				break;
+
+			ate = findAte(x * 8 + y, BLACK);
+
+			if (ate.empty())break;
+
+			board[x * 8 + y] = BLACK;
+
+			for (auto i = ate.begin(); i != ate.end(); i++)
+				board[*i] = BLACK;
+
+			sound.play();
+
+			state = WHITE_GO;
+
+			check = checkBoard(WHITE);
+
+			if (check == NO_PLACE)
+			{
+				state = GAME_END;
+				break;
+			}
+			else if (check != FINE)
+			{
+				state = BLACK_GO;
+				cout << "White no place\n";
+				check = checkBoard(BLACK);
+				break;
+			}
+
+			break;
+		case WHITE_GO:
+			//printBoard();
+
+			check = checkBoard(WHITE);
+
+			if (checkValid(x * 8 + y) != VALID)
+				break;
+
+			ate = findAte(x * 8 + y, WHITE);
+
+			if (ate.empty())break;
+
+			board[x * 8 + y] = WHITE;
+
+			for (auto i = ate.begin(); i != ate.end(); i++)
+				board[*i] = WHITE;
+
+			sound.play();
+
+			state = BLACK_GO;
+
+			check = checkBoard(BLACK);
+			if (check == NO_PLACE)
+			{
+				state = GAME_END;
+
+				break;
+			}
+			else if (check != FINE)
+			{
+				state = WHITE_GO;
+				cout << "Black no place\n";
+				check = checkBoard(WHITE);
+				break;
+			}
+			break;
+		case GAME_END:
+			break;
+		}
+	}
+
+	bool gameEnd()
+	{
+		return state == GAME_END;
+	}
+
+public:
+	int checkValid(int pos)
+	{
+		int x = pos / 8;
+		int y = pos % 8;
+
+		if (x < 0 || x>7 || y < 0 || y>7)
+			return OUT_OF_BOUND;
+
+		if (board[x * 8 + y] == WHITE|| board[x * 8 + y] == BLACK)
+			return HAVE_CHESS;
+
+		int counter = 0;
+		for (int i = -1; i <= 1; i++)
+		{
+			for (int j = -1; j <= 1; j++)
+			{
+				int tx = max(0, min(7, i + x));
+				int ty = max(0, min(7, j + y));
+
+				if (board[tx * 8 + ty] == WHITE|| board[tx * 8 + ty]== BLACK)//check around
+					counter++;
+			}
+		}
+
+		if (counter == 0)//no chess around
+			return CANT_PLACE;
+
+		return VALID;
+	}
+
+	vector<int> findAte(int pos, int type) {
+		int x = pos / 8;
+		int y = pos % 8;
+
+		vector<int> ate;
+
+		if (y - 2 > 0)//left,at least two space to eat
+		{
+			int i = 1;
+			while (y - i > 0 && board[x * 8 + y - i] == -type)
+			{
+				ate.push_back(x * 8 + y - i);
+				i++;
+			}
+			if (i > 1 && board[x * 8 + y - i] != type)
+				ate.erase(ate.end() - (i - 1), ate.end());
+		}
+		if (y + 2 < 8)//right
+		{
+			int i = 1;
+			while (y + i < 8 && board[x * 8 + y + i] == -type)
+			{
+				ate.push_back(x * 8 + y + i);
+				i++;
+			}
+			if (i > 1 && board[x * 8 + y + i] != type)
+				ate.erase(ate.end() - (i - 1), ate.end());
+		}
+		if (x - 2 > 0)//up
+		{
+			int i = 1;
+			while (x - i > 0 && board[(x - i) * 8 + y] == -type)
+			{
+				ate.push_back((x - i) * 8 + y);
+				i++;
+			}
+			if (i > 1 && board[(x - i) * 8 + y] != type)
+				ate.erase(ate.end() - (i - 1), ate.end());
+		}
+		if (x + 2 < 8)//down
+		{
+			int i = 1;
+			while (x + i < 8 && board[(x + i) * 8 + y] == -type)
+			{
+				ate.push_back((x + i) * 8 + y);
+				i++;
+			}
+			if (i > 1 && board[(x + i) * 8 + y] != type)
+				ate.erase(ate.end() - (i - 1), ate.end());
+		}
+
+		// diagonals
+		if (x - 2 > 0 && y - 2 > 0)//left-up
+		{
+			int i = 1;
+			while (x - i > 0 && y - i > 0 && board[(x - i) * 8 + (y - i)] == -type)
+			{
+				ate.push_back((x - i) * 8 + (y - i));
+				i++;
+			}
+			if (i > 1 && board[(x - i) * 8 + (y - i)] != type)
+				ate.erase(ate.end() - (i - 1), ate.end());
+		}
+		if (x - 2 > 0 && y + 2 < 8)//right-up
+		{
+			int i = 1;
+			while (x - i > 0 && y + i < 8 && board[(x - i) * 8 + (y + i)] == -type)
+			{
+				ate.push_back((x - i) * 8 + (y + i));
+				i++;
+			}
+			if (i > 1 && board[(x - i) * 8 + (y + i)] != type)
+				ate.erase(ate.end() - (i - 1), ate.end());
+		}
+		if (x + 2 < 8 && y + 2 < 8)//right-down
+		{
+			int i = 1;
+			while (x + i < 8 && y + i < 8 && board[(x + i) * 8 + (y + i)] == -type)
+			{
+				ate.push_back((x + i) * 8 + (y + i));
+				i++;
+			}
+			if (i > 1 && board[(x + i) * 8 + (y + i)] != type)
+				ate.erase(ate.end() - (i - 1), ate.end());
+		}
+		if (x + 2 < 8 && y - 2 > 0)//left-down
+		{
+			int i = 1;
+			while (x + i < 8 && y - i > 0 && board[(x + i) * 8 + (y - i)] == -type)
+			{
+				ate.push_back((x + i) * 8 + (y - i));
+				i++;
+			}
+			if (i > 1 && board[(x + i) * 8 + (y - i)] != type)
+				ate.erase(ate.end() - (i - 1), ate.end());
+		}
+		return ate;
+	}
+
+	int checkBoard(int type)
+	{
+		int counter = 0;
+		for (int i = 0; i < 8 * 8; i++)
+			if (board[i] == NO_CHESS || board[i] == CAN_GO)
+			{
+				counter++;
+				board[i] = NO_CHESS;//clear up
+			}
+		if (counter == 0)
+			return NO_PLACE;
+
+		vector<int>placeAble;
+
+		for (int i = 0; i < 8 * 8; i++)
+		{
+			if (board[i] == NO_CHESS && !findAte(i, type).empty())
+				placeAble.push_back(i);
+		}
+		//add placeable info to board
+		for (auto i = placeAble.begin(); i != placeAble.end(); i++)
+			board[*i] = CAN_GO;
+
+		if (placeAble.empty())//maybe end
+		{
+			vector<int>placeAble;
+
+			for (int i = 0; i < 8 * 8; i++)
+			{
+				if (board[i] == NO_CHESS && !findAte(i, -type).empty())
+					placeAble.push_back(i);
+			}
+			if (placeAble.empty())
+				return NO_PLACE;//end
+
+			switch (type)
+			{
+			case WHITE:
+				return WHITE_NO_PLACE;
+				break;
+			case BLACK:
+				return BLACK_NO_PLACE;
+				break;
+			}
+		}
+
+		return FINE;
+	}
+public:
+	void printBoard()
+	{
+		cout << std::setw(4) << " ";
+		for (int i = 0; i < 8; i++)
+		{
+			cout << std::setw(3) << i;
+		}
+		cout << "\n";
+		cout << std::setw(4) << " ";
+		for (int i = 0; i < 8; i++)
+		{
+			cout << std::setw(3) << "==";
+		}
+		cout << "\n";
+		for (int i = 0; i < 8; i++)
+		{
+			cout << std::setw(3) << i << "|";
+			for (int j = 0; j < 8; j++)
+			{
+				cout << std::setw(3) << board[i * 8 + j];
+			}
+			cout << "\n";
+		}
+	}
+
+	int getWinner()
+	{
+		int b = 0,w = 0;
+		for (int i = 0; i < 64 ; i++)
+		{
+			if (board[i] == WHITE)
+				w++;
+
+			if (board[i] == BLACK)
+				b++;
+		}
+
+		cout << "white:"<<w<<"\n";
+		cout << "black:" << b << "\n";
+
+		if (w == b)
+		{
+			cout << "draw\n";
+			return NO_CHESS;//draw
+
+		}
+
+		if (b > w)
+		{
+			cout << "black win!\n";
+			return BLACK;
+		}
+
+		cout << "white win!\n";
+		return WHITE;
+	}
+
+};
+
+
+
+class AI {
+public:
+	gameEngine* game;
+	int type;
+	AI(gameEngine* game, int type) :game(game), type(type) {};
+
+	void go() {
+		sf::Clock clock;
+		clock.restart();
+
+		vector<int>can_go;
+		for (int i = 0; i < 64; i++)
+		{
+			if (game->board[i] == game->CAN_GO)
+				can_go.push_back(i);
+		}
+
+		if (can_go.empty())
+			return;
+
+		int go;
+		int max_eat = 0;
+		for (auto i = can_go.begin(); i != can_go.end(); i++)
+		{
+			int s = game->findAte(*i, type).size();
+			if (s > max_eat)
+			{
+				max_eat = s;
+				go = *i;
+			}
+		}
+		
+		while (clock.getElapsedTime() < sf::seconds(0.5));
+
+		game->runGame(go / 8, go % 8);
+
+	}
+
+};
